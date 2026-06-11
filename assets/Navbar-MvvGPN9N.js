05265798db4836761a04209245b9886592ce34f1
Vishal Singh <vishal.singh980@gmail.com>
2026-06-11 15:24:34 +0530

    Updates

--- assets/Navbar-MvvGPN9N.js
+++ assets/Navbar-MvvGPN9N.js
@@ -1,4 +1,4 @@
-import{j,r as bt}from"./index-Cca7YmDx.js";import{g as St,S as yt}from"./ScrollTrigger-0lJgtTtq.js";/*!
+import{j,r as bt}from"./index-DwMinAdA.js";import{g as St,S as yt}from"./ScrollTrigger-0lJgtTtq.js";/*!
  * SplitText 3.14.2
  * https://gsap.com
  *
